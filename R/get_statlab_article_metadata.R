95db7ee4e207607a12c461eaf39a376d1a7dd1d5
Clay Ford <joe.clayton.ford@gmail.com>
2025-03-25 09:37:12 -0400

    update get metadata function

--- R/get_statlab_article_metadata.R
+++ R/get_statlab_article_metadata.R
@@ -1,52 +1,86 @@
-#' Retrieve metadata for all StatLab articles
+#' Get StatLab Article Metadata
 #'
-#' Collects the article names, authors, and tags for all articles published on the StatLab website at a given time.
+#' Collects the article names, authors, links, and tags of all currently published \href{https://library.virginia.edu/data/articles}{StatLab articles}. Function written in 2025 for StatLab website built on drupal.
 #'
-#' @return A data frame
+#' @return A data frame of article names, authors, links, and tags.
+#' @author Ethan Kadiyala
+#'
+#' @export
+#'
 #'
 #' @examples
-#' \dontrun{get_statlab_article_metadata()}
-#'
-#' @noRd
+#' \dontrun{d <- get_statlab_article_metadata()}
 get_statlab_article_metadata <- function() {
   start <- Sys.time()
-  # Initial parameters
-  query <- 'https://data.library.virginia.edu/category/statlab-articles/page/1'
-  continue <- T
-  all_articles <- list()
 
-  # Web scrape
-  while (continue == T) {
-    # Get the names and tags of the articles on a given page
-    pg <- rvest::read_html(query)
-    articles <- pg |> rvest::html_elements('article') |> rvest::html_text2()
-    article_names <- sapply(articles, function(x) stringi::stri_extract(x, regex = '^.+(?=\\nPosted)'))
-    article_tags <- lapply(articles, function(x) sort(unlist(strsplit(stringi::stri_extract(x, regex = '(?<=Tags\\: ).+$'), ', '))))
-    names(article_tags) <- article_names
-    # Add results to list, suffering through vectorization, as the list is quite small altogether
-    all_articles <- append(all_articles, article_tags)
-    # Check if process should continue on next page
-    current_pg <- as.numeric(stringi::stri_extract(query, regex = '(?<=page\\/)\\d+$'))
-    cat('\rCollected page', current_pg)
+  query <- 'https://library.virginia.edu/data/articles?page=0' # first page
+
+  pg <- rvest::read_html(query)   # read page to extract final page number
+
+  # get final page, extract hyperlink references
+  links <- pg |> rvest::html_elements('a') |> rvest::html_attr('href')
+  # find links containing '?page='
+  page_links <- links[stringr::str_detect(links, '\\?page=')]
+  # extract the numbers from the '?page=' pattern
+  page_numbers <- stringr::str_extract(page_links, '(?<=\\?page=)\\d+') |> as.numeric()
+  # get the last page number
+  last_page <- max(page_numbers, na.rm = TRUE)
+
+  # create output to append all page information to
+  out <- data.frame(title = rep(NA, times = 0),
+                    author = rep(NA, times = 0),
+                    links = rep(NA, times = 0),
+                    tags = rep(NA, times = 0))
+
+  # loop through all pages
+  current_pg <- 0 # page numbers start at 0
+  while (current_pg <= last_page) {
+
+    pg <- rvest::read_html(query)   # read page
+
+    # extract hyperlink references
+    links <- pg |> rvest::html_elements('a') |> rvest::html_attr('href')
+
+    # get article links
+    article_links <- links[stringr::str_detect(links, '/data/article') &  # extract all articles on page
+                             !stringr::str_detect(links, 'https')] # excluding articles linked in the article preview
+
+    # extract articles
+    articles <- pg |>
+      rvest::html_elements('.views-row') |>   # extracts article titles, snippets, and tags
+      rvest::html_text2() |>                  # convert to string
+      stringr::str_split(pattern = '\n')      # split by line breaks
+
+    # loop through articles and extract titles, tags, and authors
+    sub_out <- data.frame(title = rep(NA, times = length(articles)),
+                          author = NA,
+                          links = NA,
+                          tags = NA)
+
+    for (i in 1:length(articles)) {
+
+      a <- articles[[i]]   # iterate through articles
+
+      title <- a[[1]]   # extract title
+
+      all_tags <- a[[length(a)]] |> stringr::str_split(pattern = ',') |> unlist() # extract tags and split by commas
+
+      author <- all_tags[length(all_tags)]   # extract article authors
+
+      link <- paste(c('https://library.virginia.edu', article_links[i]), collapse = '')
+
+      tags <- all_tags[1:(length(all_tags)-1)] |> paste(collapse = ', ')   # extract article tags
+
+      sub_out[i,] <- list(title, author, link, tags)   # append to df
+    }
+    # append to output
+    out <- rbind(out, sub_out)
+    # update
     query <- stringi::stri_replace(query, regex = '\\d+$', replacement = current_pg + 1)
-    continue <- ifelse(httr::status_code(httr::GET(query)) == 200, T, F)
-  }
 
-  # Get valid author names
-  possible_authors <- rvest::read_html('https://data.library.virginia.edu/rds-staff/') |> rvest::html_elements(css = 'strong') |> rvest::html_text()
-  possible_authors <- stringi::stri_replace_all(str = possible_authors, regex = '\\,.+$', replacement = '')
-  # Determine each article's author
-  each_author <- sapply(all_articles, function(x) x[x %in% possible_authors])
-  # Determine non-author tags for each article
-  all_articles <- lapply(all_articles, function(x) x[(x %in% possible_authors) == F])
-  # Determine max number of non-author tags
-  max_tags <- max(sapply(all_articles, length))
-  # Structure output
-  out <- data.frame(article = names(all_articles), author = each_author, row.names = 1:length(all_articles))
-  col_names <- paste0('tag_', 1:max_tags)
-  out[col_names] <- NA
-  for (i in 1:nrow(out)) {
-    out[i, (ncol(out) - max_tags + 1):(ncol(out) - max_tags + length(all_articles[[i]]))] <- all_articles[[i]]
+    cat('\nCurrent page:', current_pg)
+
+    current_pg <- current_pg + 1
   }
 
   end <- Sys.time()
